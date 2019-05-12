4d2e01d83e293184896a097b2d5320a82dfc215f
Abagail Hudson <abagailanne@gmail.com>
2019-05-12 21:17:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,14 +36,6 @@ def adding
                   "muskets" => 2,
                   "gun powder" => 4
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
- shipping_manifest
+                 
+ return shipping_manifest
 end
